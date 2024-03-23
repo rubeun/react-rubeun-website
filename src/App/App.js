d1df166c953cc80618a9c53c416ab19f1c68098d
Rubeun Tan <github@rubeun.com>
2024-03-22 22:43:55 -0700

    add LeftComponent and RightComponent into App

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -8,9 +7,10 @@ import ParticleBackground from '../components/ParticleBackground';
 import useInterval from '../utils/useInterval';
 import useLocalStorage from '../utils/useLocalStorage';
 import Slide from '@material-ui/core/Slide';
-import { List, ListItem } from '@material-ui/core';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import LeftComponent from '../components/LeftComponent';
+import RightComponent from '../components/RightComponent';
 
 const useStyles = makeStyles((theme) => ({
   '@global': {
@@ -37,8 +37,6 @@ const useStyles = makeStyles((theme) => ({
     }
   },
   contentContainer: {
-    display: 'flex',
-    flexGrow: 1,
   },
   paperBackground: {
     background: 'url("http://portfolio.rubeun.com/img/Rubeun-Riomaggiore-Pano1a.jpg")',
@@ -135,26 +133,14 @@ const App = () => {
           <Grid item xs={12} sm={6} className={classes.contentContainer}>
             <Slide direction="up" in={pageSectionSlide.leftContent} mountOnEnter unmountOnExit>
               <Paper className={classes.paper}>
-                <Typography variant="h4">Work Experience</Typography>
-                <Typography variant="body1">
-                  <List>
-                    <ListItem>
-                      <Typography variant='h5'>EarnUp</Typography>
-                      <Typography variant='body1'>Front End Engineer</Typography>
-                      <Typography variant='body1'></Typography>
-                    </ListItem>
-                  </List>
-                </Typography>
+                <LeftComponent />
               </Paper>
             </Slide>
           </Grid>
           <Grid item xs={12} sm={6} className={classes.contentContainer}>
             <Slide direction="up" in={pageSectionSlide.rightContent} mountOnEnter unmountOnExit>
               <Paper className={classes.paper}>
-                <Typography variant="h4">Education</Typography>
-                <Typography variant="body1">
-                  Nog nglui gnaiih uaaah uh'e ilyaa stell'bsna throdor chtenff nilgh'riagl k'yarnak shugg, ah cch' goka h'bug nog ron R'lyeh n'gha hai R'lyeh, nw ebunma y-lloig kadishtu cgrah'n ch' clloig gotha f'ilyaa naR'lyeh. Ph'llll chupadgh shugg stell'bsna zhro naflshogg ron 'ai geb n'gha, Cthulhu vulgtm ee hai Nyarlathotep vulgtm y-Azathoth throd, ee y-uaaah gnaiihoth ftaghu n'ghft zhro nnnwgah'n gotha. Shub-Niggurath wgah'n h'Cthulhu naflsyha'h nglui 'fhalma yaog ron n'ghanyth uln, grah'n athg 'fhalma geb gof'nn y-hrii lw'nafhagl naflshogg shagg, tharanak li'hee stell'bsna n'ghft gotha uh'e Chaugnar Faugn h'nilgh'ri. Nnnchtenff ph'Hastur lw'nafh Dagon h'chtenff h'mg Yoggothyar n'ghft ph'gotha Cthulhu shogg, hlirgh ee mg lloig phlegeth naftaghu shagg Tsathoggua s'uhn ph'Dagon, nagotha nog nafllw'nafh uaaah s'uhn y-phlegeth ilyaaoth uln athg.
-                </Typography>
+                <RightComponent />
               </Paper>
             </Slide>
           </Grid>
