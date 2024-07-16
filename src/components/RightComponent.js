93007a84c30bec8cc0a2a7461e6c78bb4bc3a962
Rubeun Tan <github@rubeun.com>
2024-07-16 02:46:55 -0700

    add VideoPlayer into RightComponent

--- src/components/RightComponent.js
+++ src/components/RightComponent.js
@@ -1,9 +1,10 @@
 import Typography from '@material-ui/core/Typography';
 import { List, ListItem } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import VideoPlayer from './VideoPlayer';
+import earnupVid from '../App/media/new-earnup-site1.mp4';
 
 const useStyles = makeStyles((theme) => ({
-
   rightComponent: {
   },
 
@@ -19,13 +20,17 @@ const RightComponent = () => {
   const classes = useStyles();
   return (
     <div className={classes.rightComponent}>
-      <Typography variant="h4">Skills & Education</Typography>
+      <Typography variant="h4">Previous Work</Typography>
       <List>
         <ListItem>
-          <Typography variant='h5'>Proficient in:</Typography>
+          <Typography variant='h5'>EarnUp Consumer Website</Typography>
         </ListItem>
         <ListItem>
-          <Typography variant='body1'>ReactJS, JavaScript, Redux, HTML5, CSS3</Typography>
+          <Typography variant='body1'>Built the company's new consumer website from the ground up in ReactJS, ES6, Gatsby, HTML5, CSS3 wih animation, MaterialUI. 
+            I was the primary front-end developer that planned and coded the entire site working closely with the Design and Marketing teams.</Typography>
+        </ListItem>
+        <ListItem>
+          <VideoPlayer videoFile={earnupVid} />
         </ListItem>
       </List>
     </div>
