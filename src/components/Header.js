92b73cd75936e94a8f9d60579d54aa499de0faf1
Rubeun Tan <github@rubeun.com>
2024-07-16 23:11:16 -0700

    remove style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  headerText: {
-    fontSize: 56,
-  },
   rubeunIcon: {
     width: 80,
     borderRadius: '50%',
@@ -16,7 +13,7 @@ const Header = () => {
   const classes = useStyles();
   return (
     <>
-      <Typography variant="h1" className={classes.headerText}>Rubeun's Website</Typography>        
+      <Typography variant="h1">Rubeun's Website</Typography>        
       <Typography variant="h5">Front End Engineer</Typography>        
       <Typography variant="h5">San Francisco, CA</Typography>        
       <img src="http://portfolio.rubeun.com/img/favicon-96x96.png" className={classes.rubeunIcon} alt="Icon of Rubeun" />    
